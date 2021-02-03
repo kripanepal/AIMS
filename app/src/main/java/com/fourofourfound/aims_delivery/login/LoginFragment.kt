e29ee690ddef7d7898c3b5123475ac5c190a5802
Kripa Nepal <kripanepal20@gmail.com>
2021-02-03 14:44:14 -0600

    refactored LoginFragment

--- app/src/main/java/com/fourofourfound/aims_delivery/login/LoginFragment.kt
+++ app/src/main/java/com/fourofourfound/aims_delivery/login/LoginFragment.kt
@@ -1,5 +1,4 @@
 package com.fourofourfound.aims_delivery.login
-
 import android.app.AlertDialog
 import android.content.DialogInterface
 import android.content.Intent
@@ -10,7 +9,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
-import androidx.databinding.DataBindingUtil
+import android.view.inputmethod.EditorInfo
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.fourofourfound.encrypted_preferences.R
@@ -25,65 +24,46 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View{
-        binding = DataBindingUtil.inflate<FragmentLoginBinding>(
-            inflater, R.layout.fragment_login, container, false
-        )
+        binding = FragmentLoginBinding.inflate(inflater)
 
         val viewModel = LoginViewModel(requireNotNull(this.activity).application)
         binding.apply {
             this.viewModel = viewModel
-            this.lifecycleOwner = viewLifecycleOwner
+            lifecycleOwner = viewLifecycleOwner
         }
-
         binding.isLoading.setBackgroundResource(R.drawable.anim_loading)
         loadingAnimation = binding.isLoading.background as AnimationDrawable
-       if(viewModel.checkUserLoggedIn())
-       {
-           findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToHomePage())
-       }
-
-        binding.loginButton.setOnClickListener {
-           binding.apply {
-               viewModel.authenticateUser(
-                   userIdInput.text.toString(),
-                   passwordInput.text.toString()
-               )
-
-           }
-
-        }
+        if(viewModel.checkUserLoggedIn()) findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToHomePage())
 
         viewModel.loading.observe(viewLifecycleOwner,  {
-            if (it) {
-
-                loadingAnimation.start()
-            } else {
-
-                loadingAnimation.stop()
-            }
+            loadingAnimation.apply {  if (it){start()} else stop() }
         })
 
         viewModel.navigate.observe(viewLifecycleOwner,  {
             if (it) {
                 findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToHomePage())
-                viewModel.doneNavigatingToHomePage()
-            }
+                viewModel.doneNavigatingToHomePage() }
         })
-//TODO this will close the app. need to find a way to hide a keyboard
-//        binding.loginScreenConstraintLayout.setOnClickListener {
-//            hideSoftKeyboard(requireActivity())
-//        }
+
+        //TODO this will close the app. need to find a way to hide a keyboard
+        //        binding.loginScreenConstraintLayout.setOnClickListener {
+        //            hideSoftKeyboard(requireActivity())
+        //        }
 
         viewModel.errorMessage.observe(viewLifecycleOwner,  {
             val animation = AnimationUtils.loadAnimation(activity, R.anim.zoom_in_animation)
             binding.loginErrorMessage.startAnimation(animation)
-
         })
 
         binding.contactMyProvider.setOnClickListener {
             showDialog()
+      }
 
-
+        binding.passwordInput.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                viewModel.authenticateUser()
+            }
+            false
         }
         return binding.root
     }
@@ -91,25 +71,22 @@ class LoginFragment : Fragment() {
     private fun showDialog() {
         val dialogView = LayoutInflater.from(context).inflate(
             R.layout.contact_my_provider_dialog,
-            null
-        )
-        val mBuilder = AlertDialog.Builder(context)
+            null)
+        AlertDialog.Builder(context)
             .setTitle("Contact Info")
             .setView(dialogView)
             .setCancelable(false)
-            .setNegativeButton("Cancel") { dialogInterface: DialogInterface, i: Int -> dialogInterface.dismiss() }
-            .setPositiveButton("Call now") { dialogInterface: DialogInterface, i: Int ->
+            .setNegativeButton("Cancel") { dialogInterface: DialogInterface, _: Int -> dialogInterface.dismiss() }
+            .setPositiveButton("Call now") { _: DialogInterface, _: Int ->
                 startCall()
             }
-        mBuilder.show()
+        .show()
     }
-
     private fun startCall() {
-        val i = Intent(Intent.ACTION_DIAL)
-        val p = "tel:" + getString(R.string.provider_number)
-        i.data = Uri.parse(p)
-        startActivity(i)
+        val intent = Intent(Intent.ACTION_DIAL)
+        val phoneNumber = "tel:" + getString(R.string.provider_number)
+        intent.data = Uri.parse(phoneNumber)
+        startActivity(intent)
     }
-
 
 }
