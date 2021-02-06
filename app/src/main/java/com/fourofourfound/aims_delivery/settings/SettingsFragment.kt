8925646cb0a115b061274403488e779182ad4de4
Kripa Nepal <kripanepal20@gmail.com>
2021-02-06 01:30:38 -0600

    added bottom navigation

---
+++ app/src/main/java/com/fourofourfound/aims_delivery/settings/SettingsFragment.kt
@@ -0,0 +1,20 @@
+package com.fourofourfound.aims_delivery.settings
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.fourofourfound.encrypted_preferences.R
+
+class SettingsFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+}
