48f0d9c85d5cbc2e72f0d490f463e98fc17a3ce9
Kripa Nepal <kripanepal20@gmail.com>
2021-02-08 21:16:35 -0600

    updated radio buttons and input texts. Also displayed the results to user

--- app/src/main/java/com/example/location/MainActivity.kt
+++ app/src/main/java/com/example/location/MainActivity.kt
@@ -3,19 +3,16 @@ package com.example.location
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
-import android.location.Address
-import android.location.Geocoder
 import android.location.Location
 import android.location.LocationManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Looper
-import android.util.Log
+import android.widget.RadioButton
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.*
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
 
 
 
@@ -26,6 +23,10 @@ class MainActivity : AppCompatActivity() {
     lateinit var locationRequest: LocationRequest
     private val PERMISSION_REQUEST = 50
 
+
+    private var radioSelected: String = "NONE"
+    private var textEntered: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -35,8 +36,20 @@ class MainActivity : AppCompatActivity() {
         button.setOnClickListener {
             requestPermission()
             getLastLocation()
+            textEntered = text_view.text.toString()
         }
+
+        radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            (findViewById<RadioButton>(checkedId).text as String).let {
+                if (!it.isNullOrEmpty()) {
+                    radioSelected = it
+                }
+
+            }
+        }
+
     }
+
 
     //get the last known location
     private fun getLastLocation(){
@@ -47,7 +60,8 @@ class MainActivity : AppCompatActivity() {
                     if(location == null){
                         getNewLocation()
                     }else{
-                        location_text.text = "Your Current Location Latitude is: " + location.latitude + "and longitude is "  + location.longitude
+                        location_text.text =
+                            "Your Current Location Latitude is: " + location.latitude + "and longitude is " + location.longitude + "\n\n selected option " + radioSelected + "\n\nText entered " + textEntered
                     }
                 }
             }else{
@@ -59,30 +73,35 @@ class MainActivity : AppCompatActivity() {
     }
 
     @SuppressLint("MissingPermission")
-     fun getNewLocation(){
+    fun getNewLocation() {
         locationRequest = LocationRequest()
         locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
         locationRequest.interval = 0
         locationRequest.fastestInterval = 0
         locationRequest.numUpdates = 1
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
-        fusedLocationProviderClient!!.requestLocationUpdates(
-            locationRequest, locationCallback, Looper.myLooper())
+        fusedLocationProviderClient.requestLocationUpdates(
+            locationRequest, locationCallback, Looper.myLooper()
+        )
     }
 
     private val locationCallback = object: LocationCallback(){
         override fun onLocationResult(locationResult: LocationResult) {
-            var lastLocation: Location = locationResult.lastLocation
-            Log.d("Debug:","your last last location: "+ lastLocation.longitude.toString())
-            location_text.text = "Your Current Location Latitude is:" + lastLocation.latitude + "and longitude is "  + lastLocation.longitude
+            locationResult.lastLocation
         }
     }
 
     //Checks the users permission
     private fun checkPermission(): Boolean {
-        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
-                ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
-        {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED ||
+            ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
             return true
         }
         return false
@@ -97,7 +116,7 @@ class MainActivity : AppCompatActivity() {
     private fun isLocationEnabled(): Boolean{
         var locationManager= getSystemService(Context.LOCATION_SERVICE) as LocationManager
         return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
-        locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
 
     }
 
@@ -119,16 +138,16 @@ class MainActivity : AppCompatActivity() {
 //        return countryName
 //    }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        //checks the permission result
-        if(requestCode == PERMISSION_REQUEST){
-            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                Log.i("Answer", "there is a permission")
-            }
-        }
-    }
+//    override fun onRequestPermissionsResult(
+//        requestCode: Int,
+//        permissions: Array<out String>,
+//        grantResults: IntArray
+//    ) {
+//        //checks the permission result
+//        if(requestCode == PERMISSION_REQUEST){
+//            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+//                Log.i("Answer", "there is a permission")
+//            }
+//        }
+//    }
 }
