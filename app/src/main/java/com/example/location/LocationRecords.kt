8853faf36f892c8a0fb766fbd6da4cb34a248991
Nischal-Simkhada <nischalsimkhada2@gmail.com>
2021-02-15 15:22:40 -0600

    Changed teacher type to person type and fixed margins

--- app/src/main/java/com/example/location/LocationRecords.kt
+++ app/src/main/java/com/example/location/LocationRecords.kt
@@ -7,10 +7,10 @@ import java.util.function.DoubleUnaryOperator
 @Entity
 data class LocationRecords(
 
-                        var latitude: Double,
-                           var longitude: Double,
-                           var teacherType: String,
-                           var comment: String){
+    var latitude: Double,
+    var longitude: Double,
+    var person_type: String,
+    var comment: String){
     @PrimaryKey(autoGenerate = true)
     var _id: Long = 0
 }
