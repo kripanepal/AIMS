55d9942aae053f147db6c4a316258f75255196f5
Nischal-Simkhada <nischalsimkhada2@gmail.com>
2021-02-15 16:41:11 -0600

    Forms clear after pressing submit and fixed the scroll to the top

--- app/src/main/java/com/example/location/MainActivity.kt
+++ app/src/main/java/com/example/location/MainActivity.kt
@@ -7,10 +7,12 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.RadioButton
+import android.widget.ScrollView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.example.location.LocationAdapter.ViewHolder.Companion.context
 import com.example.location.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
@@ -60,6 +62,8 @@ class MainActivity : AppCompatActivity() {
 
                         if (!checkTextEmpty(textEntered)) {
                             viewModel.saveData(location, radioSelected, textEntered)
+                            recycler_view.scrollToPosition(0)
+                            edit_text.text = null
                         }
 
                     }
@@ -74,6 +78,7 @@ class MainActivity : AppCompatActivity() {
 
         viewModel.records.observe(this) {
             adapter.submitList(it)
+            recycler_view.post { recycler_view.scrollToPosition(0) }
         }
     }
 
@@ -88,8 +93,6 @@ class MainActivity : AppCompatActivity() {
         editor.putString("textEntered", edit_text.text.toString())
         editor.putInt("radioSelected", radioGroup.checkedRadioButtonId)
         editor.apply()
-
-
     }
 
     fun checkTextEmpty(text: String): Boolean {
@@ -99,6 +102,8 @@ class MainActivity : AppCompatActivity() {
         }
         return false
     }
+
+
 }
 
 
