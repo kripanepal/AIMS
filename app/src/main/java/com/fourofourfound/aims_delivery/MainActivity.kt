4bdd67928afee9c9676b56ceceef5e1b9c8f6bb8
Kripa Nepal <kripanepal20@gmail.com>
2021-02-16 17:31:34 -0600

    fixed issue with user navigating to the same screen on clicking bottom navigation bar

--- app/src/main/java/com/fourofourfound/aims_delivery/MainActivity.kt
+++ app/src/main/java/com/fourofourfound/aims_delivery/MainActivity.kt
@@ -7,45 +7,55 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.fourofourfound.aimsdelivery.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var bottomNavigationView :BottomNavigationView
-    lateinit var navController : NavController
+    lateinit var bottomNavigationView: BottomNavigationView
+    lateinit var navController: NavController
 
-        override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-         setContentView(R.layout.activity_main)
-         bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-         navController = findNavController(R.id.myNavHostFragment)
+        setContentView(R.layout.activity_main)
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+        navController = findNavController(R.id.myNavHostFragment)
         bottomNavigationView.setupWithNavController(navController)
-        navController.addOnDestinationChangedListener{ _, nd: NavDestination, _->
-                if(nd.id == R.id.loginFragment)
-                {
-                    bottomNavigationView.visibility = View.GONE
-                }
-            else
-                {
-                    bottomNavigationView.visibility = View.VISIBLE
-                }
+
+        //prevents re-navigation when user clicks on button-navigation bar  twice
+        bottomNavigationView.setOnNavigationItemReselectedListener { item ->
+            navController.currentDestination?.apply {
+                if(id === item.itemId) false
+            }
+
+
+        }
+
+        navController.addOnDestinationChangedListener { _, nd: NavDestination, _ ->
+            if (nd.id == R.id.loginFragment) {
+                bottomNavigationView.visibility = View.GONE
+            } else {
+                bottomNavigationView.visibility = View.VISIBLE
+            }
 
         }
     }
 
     //forces user to home page on back press
     override fun onBackPressed() {
-        var topDestinations = listOf(R.id.settingsFragment,R.id.ongoingDeliveryFragment,R.id.completedDeliveryFragment)
+        var topDestinations = listOf(
+            R.id.settingsFragment,
+            R.id.ongoingDeliveryFragment,
+            R.id.completedDeliveryFragment
+        )
         if (navController.currentDestination?.id in topDestinations) {
             bottomNavigationView.selectedItemId = R.id.homePage
         } else {
-           finish()
+            finish()
         }
     }
-
-
 
 
 }
