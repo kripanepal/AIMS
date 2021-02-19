dbbfb33cae39808ff3a4706c51161a840bab9817
Nischal-Simkhada <nischalsimkhada2@gmail.com>
2021-02-18 23:47:44 -0600

    Added location utils

---
+++ app/src/main/java/com/fourofourfound/aims_delivery/utils/LocationUtil.kt
@@ -0,0 +1,88 @@
+package com.fourofourfound.aims_delivery.utils
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.LocationManager
+import android.os.Looper
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
+
+class LocationUtil(var context: Context) {
+    //fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+    var fusedLocationProviderClient: FusedLocationProviderClient =
+        LocationServices.getFusedLocationProviderClient(
+            context
+        )
+    lateinit var locationRequest: LocationRequest
+    private val PERMISSION_REQUEST = 50
+
+    //var location: Location? = null
+    //lateinit var customLocation: CustomLocation
+    //get the last known location
+    fun getLastLocation() {
+        if (checkPermission()) {
+            if (isLocationEnabled()) {
+                fusedLocationProviderClient.lastLocation.addOnCompleteListener { task ->
+                    getNewLocation()
+                }
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    fun getNewLocation() {
+        locationRequest = LocationRequest()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.interval = 0
+        locationRequest.fastestInterval = 0
+        locationRequest.numUpdates = 1
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
+        fusedLocationProviderClient.requestLocationUpdates(
+            locationRequest, locationCallback, Looper.myLooper()
+        )
+    }
+
+    private val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            locationResult.lastLocation
+        }
+    }
+
+    //Checks the users permission
+    fun checkPermission(): Boolean {
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED ||
+            ActivityCompat.checkSelfPermission(
+                context,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+        return false
+    }
+
+    //asks for the user permission
+    fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            context as Activity,
+            arrayOf(
+                android.Manifest.permission.ACCESS_FINE_LOCATION,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ),
+            PERMISSION_REQUEST
+        )
+    }
+
+    //checks if device location service is enabled
+    fun isLocationEnabled(): Boolean {
+        var locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+
+    }
+}
