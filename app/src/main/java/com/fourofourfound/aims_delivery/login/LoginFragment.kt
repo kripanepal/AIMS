34cea9db20b6e42fdd08f798dca1e27e103e24d3
kripanepal <kripanepal20@gmail.com>
2021-03-04 21:42:11 -0600

    loading animation brought to front on button press

--- app/src/main/java/com/fourofourfound/aims_delivery/login/LoginFragment.kt
+++ app/src/main/java/com/fourofourfound/aims_delivery/login/LoginFragment.kt
@@ -1,5 +1,6 @@
 package com.fourofourfound.aims_delivery.login
 
+import android.app.Activity
 import android.content.Intent
 import android.graphics.drawable.AnimationDrawable
 import android.net.Uri
@@ -9,6 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
 import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -95,6 +97,10 @@ starts user authentication on click*/
             loadingAnimation.apply {
                 if (it) {
                     start()
+                    val imm =
+                        requireContext().getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(requireView().windowToken, 0)
+
                 } else stop()
             }
         })
